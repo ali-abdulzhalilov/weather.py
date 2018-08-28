2bca36b32e8ff7bf9a62abf49c8acd7ce854d273
mark-solo <clueless.magician@gmail.com>
2018-08-28 22:30:01 +0300

    implemented a function for printing weather data

--- weather.py
+++ weather.py
@@ -22,7 +22,7 @@ type_options = {'current': 'current', \
 def __init__():
 	print("am i a module")
 
-def do_a_thing(type='current', city_name=None, coordinates=None):
+def get_weather_data(type='current', city_name=None, coordinates=None):
 	print('I got %s type for %s or at %s' % (type, city_name, coordinates))
 	
 	if not city_name and not coordinates:
@@ -42,14 +42,41 @@ def do_a_thing(type='current', city_name=None, coordinates=None):
 	url_prefab = 'https://api.weatherbit.io/v2.0/'+type_options[type]
 	print(url_prefab+str(payload))
 	r = requests.get(url_prefab, params=payload)
-	print(r.json())
-	
+	return r.json()
 	
 def where_am_i():
 	import geocoder
 	
 	g = geocoder.ip('me')
 	return g.latlng
+	
+def print_weather_data(data):
+
+	count = len(data['data'])
+	for entry in data['data']:
+		print_weather_entry(entry)
+		
+def print_weather_entry(entry):
+	what_to_show = {
+		'temp': 'Temperature: {} C',
+		'app_temp': 'Feels like: {} C',
+		'wind_spd': 'Wind speed: {} m/s',
+		'wind_cdir': 'Wind direction: {}',
+		'pop': 'Probability of precipitation: {}%',
+		'vis': 'Visibility: {}km',
+		'max_temp': 'Maximum temperature: {} C',
+		'min_temp': 'Minimum temperature: {} C',
+	}
+	
+	datetime = entry['datetime'].split(':')
+	date = datetime[0]
+	time = ' ' if len(datetime) == 1 else datetime[1] + ":00 "
+	
+	print('----- %s %s-----' % (date, time))
+	for thing in what_to_show:
+		if thing in entry:
+			print(what_to_show[thing].format(entry[thing]))
+	print()
 
 if (__name__=='__main__'):
 	import argparse
@@ -62,4 +89,5 @@ if (__name__=='__main__'):
 	
 	args = parser.parse_args()
 	
-	do_a_thing(args.type, args.city_name, args.coordinates)
+	data = get_weather_data(args.type, args.city_name, args.coordinates)
+	print_weather_data(data)
